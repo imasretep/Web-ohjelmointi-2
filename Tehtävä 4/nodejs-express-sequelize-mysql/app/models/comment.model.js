0ec7d3b6ecde1e2fb5c897b7f762ce7431b1e736
Sami <samipetersalmi@gmail.com>
2024-10-27 16:52:52 +0200

    Delete Tehtävä 4 directory

--- Tehtävä 4/nodejs-express-sequelize-mysql/app/models/comment.model.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Comment = sequelize.define("comment", {
-        comment: {
-            type: Sequelize.STRING
-        },
-    });
-    return Comment;
-};
